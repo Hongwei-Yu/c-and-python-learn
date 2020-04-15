ad4dff88c46a1bad791b0acb1e4a514a0d3ecdc7
Hongwei-Yu <62824819+Hongwei-Yu@users.noreply.github.com>
2020-04-15 17:25:32 +0800

    Create 爬取网页图片的通用框架.py

---
+++ 爬取网页图片的通用框架.py
@@ -0,0 +1,14 @@
+import requests
+def getHMLTEXT(url):
+    try:
+        r=requests.get(url,timeout=30)#（url）
+        r.raise_for_status()#如果状态不是200，引发HTTPError异常
+        r.encoding=r.apparent_encoding
+        return r.text
+    except:
+        return "产生异常"#产生HTTPError异常就会返回到这个expect语句
+if __name__=="__main__":
+    url="https://item.jd.com/2967929.html"
+    print(getHMLTEXT(url))
+#这是爬取网页的通用代码框架
+
